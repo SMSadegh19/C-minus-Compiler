02eb33ade8ab073cba9c89c2ac9a8b0a8f794f46
amirmohammad <am.ghasemi@pinket.com>
2020-11-05 12:44:49 +0330

    run_test.sh added to git for phase 2

---
+++ phase 2/Test Area/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "" > log.txt
+echo "" > brief_results.txt
+for dir in ../PA2_input_output_samples/*; do
+    cp "${dir}/input.txt" ./input.txt
+    echo "${dir}"
+    python3 ../../Parser.py
+    printf "\n\n\n\n=====================================>>>>> Running Test ${dir}...\n" >> log.txt
+    printf "\n\n=====================================>>>>> Running Test ${dir}...\n" >> brief_results.txt
+    printf "\n\n              *** parse_tree.txt diffrences ***\n" >> log.txt
+    diff -s -y -B -W 250 -w parse_tree.txt "${dir}/parse_tree.txt" >> log.txt
+    diff -y -B -W 250 -w -q parse_tree.txt "${dir}/parse_tree.txt" >> brief_results.txt
+    printf "\n\n              *** syntax_errors.txt diffrences ***\n" >> log.txt
+    diff -s -y -B -W 250 -w syntax_errors.txt "${dir}/syntax_errors.txt" >> log.txt
+    diff -y -B -W 250 -w -q syntax_errors.txt "${dir}/syntax_errors.txt" >> brief_results.txt
+done
